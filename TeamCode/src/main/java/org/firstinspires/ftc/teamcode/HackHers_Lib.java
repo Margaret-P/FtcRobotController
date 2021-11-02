9f805aa29d1895af396e330820d33323754e7162
Margaret-P <mspotagal@gmail.com>
2021-11-02 18:20:04 -0400

    Added beginnings of personal drive code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HackHers_Lib.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+public class HackHers_Lib {
+    public DcMotor frontLeft;
+    public DcMotor frontRight;
+    public DcMotor backLeft;
+    public DcMotor backRight;
+    public HackHers_Lib(DcMotor fl, DcMotor fr, DcMotor bl, DcMotor br){
+        this.frontLeft= fl;
+        this.frontRight = fr;
+        this.backLeft= bl;
+        this.backRight = br;
+    }
+
+    public void driveRaw(float fl, float fr, float bl, float br){
+        frontLeft.setPower(fl);
+        frontRight.setPower(fr);
+        backLeft.setPower(bl);
+        backRight.setPower(br);
+    }
+
+
+}
+
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PaulMath.java
@@ -0,0 +1,5 @@
+package org.firstinspires.ftc.teamcode;
+
+public class PaulMath {
+    public static float[] omniCalc(float verticalPower)
+}

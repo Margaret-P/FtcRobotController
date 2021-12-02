de1109643a02a627f612cdff118964bac73fafe4
Margaret-P <mspotagal@gmail.com>
2021-12-02 14:52:22 -0500

    Create TestTeleOp.java
    Added test teleop without new code for troubleshooting

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestTeleOp.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@com.qualcomm.robotcore.eventloop.opmode.TeleOp
+public abstract class TestTeleOp extends OpMode {
+    private HackHers_Lib everything;
+    DcMotor fL = hardwareMap.get(DcMotor.class, "fl");
+    DcMotor fR = hardwareMap.get(DcMotor.class, "fR");
+    DcMotor bL = hardwareMap.get(DcMotor.class, "bl");
+    DcMotor bR = hardwareMap.get(DcMotor.class, "bR");
+    DcMotor dw = hardwareMap.get(DcMotor.class, "dw");
+
+    public void init() {
+
+        everything = new HackHers_Lib(fL, fR, bL, bR, dw);
+    }
+
+    @Override
+    public void loop() {
+            everything.omniDrive(gamepad1.right_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x);
+    }
+}
